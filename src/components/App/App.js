0fa32a703ca94a66d3bb455f8cbbe21e038d059b
Chaoching Vang <chaoching.vang@gmail.com>
2021-11-07 12:56:05 -0600

    styled details page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,14 +12,17 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
+      <div className="header">
+      <Typography variant="h2">The Movies Saga!</Typography>
+      </div>
       <Router>        
         <Route path="/" exact>
-          <MovieList />
+            <MovieList />
         </Route>
         
         {/* Details page */}
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
+import './MovieDetails.css'
 
+//mui imports
+import { Box, Typography } from '@mui/material'
+import Card from '@mui/material/Card';
 
 function MovieDetails() {
     const dispatch = useDispatch();
@@ -29,18 +33,40 @@ function MovieDetails() {
     //if no movie was selected, renders and tell user to select a movie to see details
     return (<>{movie === `noMovie` ?
         <>
-            <h1>No movie selected!</h1>
+            <Typography variant="h1">No movie selected!</Typography>
             <h3>Please go back and select a movie to view its details!</h3>
             <button onClick={handleClick}>Back</button>
         </> :
         
         <>
-            <img src={movie.poster} />
-            <h1>{movie.title}</h1>
-            <p>Genres: <br />
-                - {genres.map(genre => (<> {genre.name} -</>))}</p>
-            <p>{movie.description}</p>
-            <button onClick={handleClick}>Back</button>
+            <Box sx={{
+                width: 300,
+                height: 400,
+                margin: 'auto',
+            }}>
+                <Card
+                    variant="outlined"
+                    sx={{
+                        width: 300,
+                        height: 400,
+                        margin: 'auto',
+                        backgroundColor: 'beige',
+                        border: 'solid black'
+                    }}>
+                    <img
+                        className="posterImg"
+                        src={movie.poster} />
+                    <h2>{movie.title}</h2>
+                </Card>
+                </Box>
+            <div className="details">
+                <Typography variant="body1">
+                <p>Genres: <br />
+                    - {genres.map(genre => (<> {genre.name} -</>))}</p>
+                <p>{movie.description}</p>
+                <button onClick={handleClick}>Back</button>
+                </Typography>
+            </div>
        </>}
     </>)
 }
